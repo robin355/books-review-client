1f22709817442be92c627a7ac2545da3c8ab2972
robin mahmud <robinmahmud358@gmail.com>
2022-11-14 11:35:15 +0600

    banner added

---
+++ src/pages/Home/Home/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import cover from '../../../../Assets/Images/cover.jpg'
+const Banner = () => {
+    return (
+        <div>
+            <div className="hero" style={{ background: `url(${cover})` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Hello Books Lovers</h1>
+                        <p className="mb-5">Bibliophilia or bibliophilism is the love of books. A bibliophile or bookworm is an individual who loves and frequently reads books.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
